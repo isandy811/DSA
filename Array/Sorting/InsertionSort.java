aaa1adc859ba977c4750a028ee767e946b6c6f92
Sandhya <sandhyas11011@gmail.com>
2025-01-22 14:39:16 +0530

    Create InsertionSort.java

---
+++ Array/Sorting/InsertionSort.java
@@ -0,0 +1,25 @@
+package Array.Sorting;
+import java.util.Scanner;
+
+public class InsertionSort{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int curr=arr[i];
+            int j=i-1;
+            while(j>=0 &&curr<arr[j]){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=curr;
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
